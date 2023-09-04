39e27162e8b068d3ec28f7a7fca3f0b242a0d273
FLCordis <flavio.cordis@hotmail.com>
2023-09-04 12:02:03 -0300

    FIX: Script fix and new filename format per date

--- script.js
+++ script.js
@@ -6,33 +6,28 @@ console.log("Carregando o bot para o Feedz! 🤖");
 
 (async function robo(){
     //Executa o navegador e abre uma página nova | {headless: false} para ver o navegador abrindo
-    const browser = await puppeteer.launch({headless: "new"});
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
 
     //Carrega a página e preenche os dados
     await page.goto("https://app.feedz.com.br/");
     await page.type('[name="login_email"]', process.env.UNSPLASH_EMAIL);
-    await page.type('[name="login_password"]', process.env.UNSPLASH_PASSWORD);
-    
-    await page.screenshot({path: 'login.png'});
+    await page.type('#passInput', process.env.UNSPLASH_PASSWORD);
 
     //Ele espera a navegação terminar de carregar e clica no botão
-    await Promise.all([
-        page.waitForNavigation(),
-        page.click('#enter-login')
-    ]);
+    await page.click('#enter-login');
 
     //Espera a página carregar
     await page.waitForNavigation();
 
-    //Ele seleciona o botão e seleciona o "mood4" que é seu humor diário
-    await page.waitForSelector('img[src="/assets/images/mood-4.png"]');
-    await page.click('img[src="/assets/images/mood-4.png"]');
+    //Ele seleciona o botão e seleciona o "mood5" que é seu humor diário
+    await page.waitForSelector('img[src="/assets/images/mood-5.png"]');
+    await page.click('img[src="/assets/images/mood-5.png"]');
 
     //Ele seleciona o botão de enviar e clica, tirando uma screenshot depois pra ver se foi tudo certo
     await page.waitForSelector('#fdz-btn-send-mood');
     await page.click('#fdz-btn-send-mood');
 
-    await page.screenshot({path: 'status.png'});
+    await page.screenshot({path: `img/status_${(new Date().toJSON().slice(0,10))}.png` });
     await browser.close();
 })();
